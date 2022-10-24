1d8c8b6fe667d2554bff58a40c99e08a609ef96a
Huang Yu <huangyub@uniontech.com>
2022-10-24 18:11:37 +0800

    add java parse and default python file

--- until/javaparser.py
+++ until/javaparser.py
@@ -1,12 +1,17 @@
 from until.parser import Parser
 from until.globallog import log
+import javac_parser
 
 class JavaParser(Parser):
     def __init__(self):
         super(JavaParser, self).__init__()
+        self.__java_parser = javac_parser.Java()
 
     def doParse(self, storage, files, file):
         super(JavaParser, self).doParse(storage, files, file)
+        f = open(file, 'r', encoding='utf8')
+        res = self.__java_parser.lex(f.read())
+        print(res)
 
     def language(self):
         return ['Java']
--- until/pythonparser.py
+++ until/pythonparser.py
@@ -1,13 +1,23 @@
 from until.parser import Parser
 from until.globallog import log
+import parser
 
 class PythonParser(Parser):
 
     def __init__(self):
         super(PythonParser, self).__init__()
+    def load_suite(self, source_string):
+        st = parser.suite(source_string)
+        return st, st.compile()
+
+    def load_expression(self, source_string):
+        st = parser.expr(source_string)
+        return st, st.compile()
 
     def doParse(self, storage, files, file):
         super(PythonParser, self).doParse(storage, files, file)
+        f = open(file, 'r', encoding='utf8')
+        print(self.load_suite(f.read()))
 
     def language(self):
         return ['Python']
